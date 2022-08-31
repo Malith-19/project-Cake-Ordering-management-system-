4513481188d41c7610f3922bedef8f09ee633ea1
RdyhALzbydy <86219640+RdyhALzbydy@users.noreply.github.com>
2022-08-31 07:41:07 +0300

    Final Project

---
+++ project(Cake Orderingmanagement system)/project(Cake Orderingmanagement system)/project_in/JavaScript/buy.js
@@ -0,0 +1,8 @@
+
+
+function MyFunction()
+{
+	 alert("Hello people!");
+var r=confirm("Thank you for your purchase from our store");
+
+}
---
+++ project(Cake Orderingmanagement system)/project(Cake Orderingmanagement system)/project_in/JavaScript/script1.js
@@ -0,0 +1,9 @@
+
+
+for(i=1;i<=3;i++){ <!-- here javascript loop -->
+alert("Hello World");}
+var website=new Object(); <!-- create javascript object-->
+website.Mywebsite="Mywebsite is New";
+alert(website.Mywebsite);
+
+<!-- here javascript object-->
